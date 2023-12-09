0e0cabc3f8f952f500f18103bf7f60477a179d66
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-12-08 20:37:17 -0600

    added more stuff

--- environment/env.py
+++ environment/env.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import math
 
 class Grid_Environment:
     def __init__(self, size_x: int, size_y: int, rand_goal: bool=False, rand_start: bool=False):
@@ -8,6 +9,7 @@ class Grid_Environment:
         self.max_y = size_y - 1 
         self.done = False
         self.episode_length = 0
+        self.reward = 0
         if (rand_start):
             self.x, self.y = random.randint(1,self.max_x), random.randint(1,self.max_x)
         else:
@@ -18,47 +20,59 @@ class Grid_Environment:
             self.goal = [self.max_x, self.max_y]
         self.state = [self.x, self.y]
 
-        def reset(self):
-            self.x, self.y = 0, 0 
-            self.done = False
-            self.episode_length = 0
-            self.state = [self.x, self.y]
-            return [self.x, self.y]
-        
-        def action_space(self):
-            return self.actions
+    def reset(self):
+        self.x, self.y = 0, 0
+        self.done = False
+        self.episode_length = 0
+        self.reward = 0
+        self.state = [self.x, self.y]
+        return [self.x, self.y]
 
-        def step(self, action):
-            if (self.state == self.goal or self.episode_length > 200):
-                self.done = True
-                return np.array(self.state), self.reward, self.done, self.episode_length
-            
-            self.action = action
-            self.reward = self.get_reward()
-            self.state = self.take_action()
-            self.episode_length += 1
+    def action_space(self):
+        return self.actions
 
-            if (self.state == self.goal or self.episode_length > 200):
-                self.done = True
-                return np.array(self.state), self.reward, self.done, self.episode_length
-            
-        def take_action(self):
-            if(self.action == "left" & self.x == 0) or (self.action == "right" & self.x == self.max_x):
-                self.x = self.x
-            elif(self.action == "left"):
-                self.x -= 1
-            elif(self.action == "right"):
-                self.x += 1
-            else:
-                self.x = self.x
-            if(self.action == "down" & self.y == 0) or (self.action == "up" & self.y == self.max_x):
-                self.y = self.y
-            elif(self.action == "down"):
-                self.y -= 1
-            elif(self.action == "up"):
-                self.y += 1
-            else:
-                self.y = self.y
+    def step(self, action):
+        if (self.state == self.goal or self.episode_length > 200):
+            self.done = True
+            return np.array(self.state), self.reward, self.done, self.episode_length
+
+        self.action = action
+        self.reward += self.get_reward()
+        self.state = self.take_action()
+        self.episode_length += 1
+
+        if (self.state == self.goal or self.episode_length > 200):
+            self.done = True
+            return np.array(self.state), self.reward, self.done, self.episode_length
+
+    def get_reward(self):
+        if(self.episode_length > 200):
+            reward = -10
+        elif(self.state == self.goal):
+            reward = 10
+
+    def take_action(self):
+        if (self.action == "left" and self.x == 0) or (self.action == "right" and self.x == self.max_x):
+            self.x = self.x
+        elif (self.action == "left"):
+            self.x -= 1
+        elif (self.action == "right"):
+            self.x += 1
+        else:
+            self.x = self.x
+        if (self.action == "down" and self.y == 0) or (self.action == "up" and self.y == self.max_x):
+            self.y = self.y
+        elif (self.action == "down"):
+            self.y -= 1
+        elif (self.action == "up"):
+            self.y += 1
+        else:
+            self.y = self.y
+
+        return [self.x, self.y]
+
+
+
         
         
         
--- environment/game.py
+++ environment/game.py
@@ -1,11 +1,15 @@
 import pygame as pg
 import sys
+from env import Grid_Environment
+import math
 
 # Initialize pg
 pg.init()
 
-WIDTH, HEIGHT = 600, 600
-GRID_SIZE = 30
+env = Grid_Environment(4, 4)
+
+WIDTH, HEIGHT = 1000, 1000
+GRID_SIZE = int(1000 / (env.max_x + 1))
 AGENT_COLOR = (255, 0, 0)
 OBJECTIVE_COLOR = (0, 255, 0)
 GRID_COLOR = (255, 255, 255)
@@ -13,8 +17,8 @@ GRID_COLOR = (255, 255, 255)
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Grid Game")
 
-agent_pos = [0, 0]
-objective_pos = [10, 10]
+agent_pos = env.state
+objective_pos = env.goal
 
 running = True
 while running:
@@ -23,24 +27,24 @@ while running:
             running = False
 
     keys = pg.key.get_pressed()
-    if keys[pg.K_LEFT] and agent_pos[0] > 0:
-        agent_pos[0] -= 1
-    if keys[pg.K_RIGHT] and agent_pos[0] < WIDTH // GRID_SIZE - 1:
-        agent_pos[0] += 1
-    if keys[pg.K_UP] and agent_pos[1] > 0:
-        agent_pos[1] -= 1
-    if keys[pg.K_DOWN] and agent_pos[1] < HEIGHT // GRID_SIZE - 1:
-        agent_pos[1] += 1
+    if keys[pg.K_a]:
+        env.step("left")
+    if keys[pg.K_d]:
+        env.step("right")
+    if keys[pg.K_s]:
+        env.step("down")
+    if keys[pg.K_w]:
+        env.step("up")
+    agent_pos = env.state
 
     screen.fill(GRID_COLOR)
     for x in range(0, WIDTH, GRID_SIZE):
-        pg.draw.line(screen, (0, 0, 0), (x, 0), (x, HEIGHT))
+        pg.draw.line(screen, (0, 0, 0), (x, HEIGHT), (x, 0), 1)
     for y in range(0, HEIGHT, GRID_SIZE):
-        pg.draw.line(screen, (0, 0, 0), (0, y), (WIDTH, y))
+        pg.draw.line(screen, (0, 0, 0), (0, HEIGHT - y), (WIDTH, HEIGHT - y), 1)
 
-    pg.draw.rect(screen, AGENT_COLOR, (agent_pos[0] * GRID_SIZE, agent_pos[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-    pg.draw.rect(screen, OBJECTIVE_COLOR, (objective_pos[0] * GRID_SIZE, objective_pos[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-
+    pg.draw.rect(screen, AGENT_COLOR, ((agent_pos[0] * GRID_SIZE), (HEIGHT - (agent_pos[1] * GRID_SIZE) - GRID_SIZE), GRID_SIZE, GRID_SIZE))
+    pg.draw.rect(screen, OBJECTIVE_COLOR, (objective_pos[0] * GRID_SIZE, (HEIGHT - (objective_pos[1] * GRID_SIZE) - GRID_SIZE), GRID_SIZE, GRID_SIZE))
     pg.display.flip()
 
     pg.time.Clock().tick(10)
