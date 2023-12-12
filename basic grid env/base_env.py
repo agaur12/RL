5bfd351904aaa87e8a5f5bc2b874e4dbb7303455
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-12-12 18:36:15 +0000

    added some stuff

--- basic grid env/base_env.py
+++ basic grid env/base_env.py
@@ -4,6 +4,8 @@ import math
 import gym
 from gym import spaces
 import math
+import pygame as pg
+import sys
 
 
 class GridEnvironment(gym.Env):
@@ -99,4 +101,56 @@ class GridEnvironment(gym.Env):
             self.y = self.y
 
         return [self.x, self.y]
+    
+    def render():
+        pg.init()
+
+        WIDTH, HEIGHT = 1000, 1000
+        GRID_SIZE = int(1000 / (self.max_x + 1))
+        AGENT_COLOR = (255, 0, 0)
+        OBJECTIVE_COLOR = (0, 255, 0)
+        GRID_COLOR = (255, 255, 255)
+
+        screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Grid Game")
+
+        agent_pos = self.state
+        objective_pos = self.goal
+
+
+        running = True
+        while running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+
+            keys = pg.key.get_pressed()
+            if keys[pg.K_a]:
+                self.step(3)
+            if keys[pg.K_d]:
+                self.step(1)
+            if keys[pg.K_s]:
+                self.step(2)
+            if keys[pg.K_w]:
+                self.step(0)
+            agent_pos = self.state
+
+            screen.fill(GRID_COLOR)
+            for x in range(0, WIDTH, GRID_SIZE):
+                pg.draw.line(screen, (0, 0, 0), (x, HEIGHT), (x, 0), 1)
+            for y in range(0, HEIGHT, GRID_SIZE):
+                pg.draw.line(screen, (0, 0, 0), (0, HEIGHT - y), (WIDTH, HEIGHT - y), 1)
+
+            pg.draw.rect(screen, AGENT_COLOR, ((agent_pos[0] * GRID_SIZE), (HEIGHT - (agent_pos[1] * GRID_SIZE) - GRID_SIZE), GRID_SIZE, GRID_SIZE))
+            pg.draw.rect(screen, OBJECTIVE_COLOR, (objective_pos[0] * GRID_SIZE, (HEIGHT - (objective_pos[1] * GRID_SIZE) - GRID_SIZE), GRID_SIZE, GRID_SIZE))
+            pg.display.flip()
+
+            if agent_pos == objective_pos:
+                self.reset()
+
+            pg.time.Clock().tick(10)
+
+        pg.quit()
+        sys.exit()
+
 
